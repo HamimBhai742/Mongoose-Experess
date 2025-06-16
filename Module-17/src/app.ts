84e2224e0ad26d2e9dc79f10fde04dff5c8a8de4
HamimBhai742 <omagootruelove147@gmail.com>
2025-06-16 11:22:52 +0600

    Update folder stracture and  module 17 er 17.6  complete

--- Module-17/src/app.ts
+++ Module-17/src/app.ts
@@ -1,6 +1,6 @@
 import express, { Application } from 'express';
 const app: Application = express();
-import { router } from './controllers/noteControllres';
+import { router } from './app/controllers/noteControllres';
 app.use(express.json());
 
 // const noteRouter = require('./controllers/noteControllres');
